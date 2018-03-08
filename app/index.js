68523c1b999b24ebfe20a6ddf687defb810e8d21
Ankit Chawla <ankitchawla777@gmail.com>
2018-03-08 18:13:37 +0530

    added snooze and answer button

--- app/index.js
+++ app/index.js
@@ -8,8 +8,7 @@ const app = electron.app
 
 // Module to create native browser window.
 
-const BrowserWindow = electron.BrowserWindow
-
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -19,9 +18,9 @@ function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     frame: false,
-    width: 400,
+    width: 600,
     transparent: true,
-    height: 300,
+    height: 600,
     alwaysOnTop: true,
     toolbar: false,
     titleBarStyle: 'hidden',
@@ -30,7 +29,9 @@ function createWindow() {
     fullscreenable: false,
     simpleFullscreen: false,
     skipTaskbar: true,
-    title: ''
+    title: '',
+    x:0,
+    y:0
   });
  
   win.loadURL(url.format({
@@ -41,30 +42,20 @@ function createWindow() {
 
   // Emitted when the window is closed.
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
--- app/service.js
+++ app/service.js
@@ -1,18 +1,58 @@
+const electron = require('electron');
+const path = require('path');
+const $ = require('jquery')
+const BrowserWindow = electron.remote.BrowserWindow;
+const socket = io('http://localhost:2018');
+const { remote } = require('electron')
 
-const socket=io('http://localhost:2018');
-console.log("calling api");
-let msgs = document.querySelector('div');
-// axios.get('http://localhost:3000/users').then( (res) => {
-//   msgs.innerHTML="got a new msg";
-//   msgs.hidden=false;
-//  })
-socket.on('newQuestion',(res)=>{
-  msgs.innerHTML="got a new msg";
-  setTimeout(()=>{
-    msgs.innerHTML=res.ques;
-  },300)
-  msgs.hidden=false;
+const msgs = document.getElementById('msg-bubble');
+const msgtxt = document.getElementById('msg')
+const opt = document.getElementById('options')
+const snooze = document.getElementById('snz');
+const answer = document.getElementById('ans');
+const currentWin=remote.BrowserWindow.getFocusedWindow();
+let ques;
+socket.on('newQuestion', (res) => {
+  msgs.hidden = false;
+  msgtxt.innerHTML = "got a new msg";
+  setTimeout(() => {
+    ques = res.ques;
+    msgtxt.innerHTML = res.ques;
+    opt.hidden = false;
+  }, 3000)
 })
 
+snooze.addEventListener('click', (event) => {
+  var pos=currentWin.getPosition();
+  if((pos[0]<screen.width-600) || (pos[1]<screen.height-600)){
+    pos[0]=screen.width-600;
+    pos[1]=0;
+  }
+  else if((pos[0]>=screen.width-600) || (pos[1]<screen.height-600)){
+    pos[0]=screen.width-600;
+    pos[1]=screen.height-600;
+  }
+  else if((pos[0]>=screen.width-600) || (pos[1]>=screen.height-600)){
+    pos[0]=0;
+    pos[1]=screen.height-600;
+  }
+  else if((pos[0]<screen.width-600) || (pos[1]>=screen.height-600)){
+    pos[0]=screen.width-600;
+    pos[1]=0;
+  }
+  console.log("snz btn clicked")
+  currentWin.hide();
+  setTimeout(() => {
+    currentWin.setPosition(pos[0],pos[1])
+    currentWin.show();
+  }, 3000)
 
- 
+})
+answer.addEventListener('click', (event) => {
+  console.log("ans btn clicked");
+  const modalPath = path.join(__dirname, 'answer.html')
+  let win = new BrowserWindow({ width: 400, height: 200,frame:false,alwaysOnTop:true })
+  win.on('close', function () { win = null })
+  win.loadURL(modalPath)
+  win.show()
+});
