94febe905614e1d8add84f643faffed5af77e7c9
aakashvermaee <aakashvermaee@gmail.com>
2018-08-24 13:20:33 +0530

    removed unncecssary file

--- models/db.js
+++
@@ -1,7 +0,0 @@
-require('../config/config');
-const mongoose=require('mongoose');
-mongoose.promise= require('bluebird');
-
-mongoose.connect(process.env.MONGODB_URI).catch((e)=>console.log(e));
-
-module.exports={mongoose};
